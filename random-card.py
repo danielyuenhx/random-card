c405b5d95d90870d33dfe26c0231273a87931fe5
danielyuenhx <yuendaniel33@gmail.com>
2022-07-22 22:37:55 +0800

    completed drawing card functionality with images

--- random-card.py
+++ random-card.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import random
+from PIL import ImageTk, Image
 
 # create window
 root = Tk()
@@ -15,6 +16,23 @@ for num in num_list:
     for shape in shape_list:
         deck.append((num, shape))
 
+def get_image(card):
+    # use 2nd image if picture card
+    if card[0] in ('jack','queen','king'):
+        file_name = card[0] + '_of_' + card[1] + '2.png'
+    else:
+        file_name = card[0] + '_of_' + card[1] + '.png'
+
+    # find and open image
+    card_img = Image.open('images/' + file_name)
+    
+    card_img_resized = card_img.resize((150,218))
+
+    global out_card_img
+    out_card_img = ImageTk.PhotoImage(card_img_resized)
+
+    return out_card_img
+
 def draw():
     # num = random.choice(num_list)
     # shape = random.choice(shape_list)
@@ -23,9 +41,17 @@ def draw():
     card = random.choice(deck)
     deck.remove(card)
 
-    root.title('Random Card Picker - ' + str(len(deck)) + ' cards left')
-    card_frame.config(text=card)
-    card_label.config(text=card)
+    if len(deck) > 0:
+        root.title('Random Card Picker - ' + str(len(deck)) + ' cards left')
+        card_frame.config(text=card[0].capitalize() + ' of ' + card[1].capitalize())
+
+        card_img = get_image(card)
+        card_label.config(image=card_img)
+    else:
+        text = 'No cards left!'
+        card_label.config(text=text)
+        root.title('Random Card Picker - ' + text)
+        button.config(state=DISABLED)
 
     return card
 
@@ -33,7 +59,7 @@ root_frame = Frame(root, bg="green")
 root_frame.pack(pady=20)
 
 # frames for cards
-card_frame = LabelFrame(root_frame, text="", bd=0)
+card_frame = LabelFrame(root_frame, text='', bd=0)
 card_frame.grid(row=0, column=0, ipadx=20)
 
 # adding cards into frames
