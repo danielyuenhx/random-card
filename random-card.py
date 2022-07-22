f11dec1a4d02ea36a469be11fc58cdc0cbea100d
danielyuenhx <yuendaniel33@gmail.com>
2022-07-22 22:11:53 +0800

    added drawing card functionality

--- random-card.py
+++ random-card.py
@@ -7,21 +7,33 @@ root.title("Random Card Picker")
 root.geometry("900x500")
 root.configure(background="green")
 
-def draw():
-    num_list =['2','3','4','5','6','7','8','9','10','jack','queen','king','ace']
-    shape_list =['spades','hearts','clubs','diamonds']
-    num = random.choice(num_list)
-    shape = random.choice(shape_list)
-    card = num + '_of_' + shape
-    # return card
+num_list =['2','3','4','5','6','7','8','9','10','jack','queen','king','ace']
+shape_list =['spades','hearts','clubs','diamonds']
+deck = []
 
+for num in num_list:
+    for shape in shape_list:
+        deck.append((num, shape))
+
+def draw():
+    # num = random.choice(num_list)
+    # shape = random.choice(shape_list)
+    # card = num + '_of_' + shape
+    # return card
+    card = random.choice(deck)
+    deck.remove(card)
+
+    root.title('Random Card Picker - ' + str(len(deck)) + ' cards left')
+    card_frame.config(text=card)
     card_label.config(text=card)
+
+    return card
 
 root_frame = Frame(root, bg="green")
 root_frame.pack(pady=20)
 
 # frames for cards
-card_frame = LabelFrame(root_frame, text="test", bd=0)
+card_frame = LabelFrame(root_frame, text="", bd=0)
 card_frame.grid(row=0, column=0, ipadx=20)
 
 # adding cards into frames
@@ -45,6 +57,7 @@ button.pack(pady=20)
 # myButton = Button(root, text="Draw Card", padx=50, pady=50)
 # myButton.grid(row=0, column=0)
 
-# create event loop to keep GUI on
-root.mainloop()
+if __name__ == "__main__":
+    # create event loop to keep GUI on
+    root.mainloop()
 
